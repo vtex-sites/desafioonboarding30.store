732a9fb19f788143cc79329d23fa40749a39444e
Eduardo Barbosa <eduardo.barbosa@vtex.com.br>
2023-09-09 21:39:08 -0300

    quebra tudo

---
+++ src/components/CustomBuyButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button as UIButton, ButtonProps } from '@faststore/ui'
+ 
+export function CustomBuyButton(props: ButtonProps) {
+  return (
+    <UIButton
+      {...props}
+      variant="primary"
+      onClick={(event) => {
+        alert('Edu maravilhoso!')
+        props.onClick?.(event)
+      }}
+    >
+      New Buy Button
+    </UIButton>
+  )
+}
--- src/components/overrides/ProductDetails.tsx
+++ src/components/overrides/ProductDetails.tsx
@@ -1,10 +1,12 @@
 import { SectionOverride } from '@faststore/core/src/typings/overrides'
+import { CustomBuyButton } from '../CustomBuyButton'
  
 const SECTION = 'ProductDetails' as const
  
 const override: SectionOverride = {
   section: SECTION,
-  components: {BuyButton: { props: { size: 'regular', iconPosition: 'right' } },}
+  components: {
+    BuyButton: { Component: CustomBuyButton }}
 }
  
 export { override }
